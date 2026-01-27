e3da3c5fb75bcf9d7faa1e70ce7d0683f6b36bb6
Faiyaz <sifathhossain115@gmail.com>
2026-01-27 17:21:12 +0600

    Fix: global.css second import removed

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,10 @@ export default function Home() {
           <section className="container mx-auto px-4 py-32">
             <div>
               <h1 className="text-black">A better way to track your job application.</h1>
-              <p className="text-gray-700">Capture, organize, and manage your job in one place.</p>
+              <p className="text-muted-foreground">Capture, organize, and manage your job in one place.</p>
             </div>
+              <button>Start fro free</button>
+              <p>Free foreven. No credit card required</p>
           </section>
         </main>
       </div>
