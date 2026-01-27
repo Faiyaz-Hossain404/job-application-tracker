799175024bae491d013016be45ff9f95ac55686e
Faiyaz <sifathhossain115@gmail.com>
2026-01-27 16:03:52 +0600

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="flex min-h-screen flex-col bg-white">
+        <main className="flex1">
+          {/* Hero Section */}
+          <section className="container mx-auto px-4 py-32">
+            <div>
+              <h1 className="text-black">A better way to track your job application.</h1>
+              <p className="text-gray-700">Capture, organize, and manage your job in one place.</p>
+            </div>
+          </section>
+        </main>
+      </div>
+  );
+}
