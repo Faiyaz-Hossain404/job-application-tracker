c4882f6bc8507f75268b74a28113f5d481eed5f2
Faiyaz <sifathhossain115@gmail.com>
2026-02-08 00:43:57 +0600

    Feat: mongoose db type definition for collections initiated

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please provide the MONGODB_URI environment variable insdie .env",
+  );
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
