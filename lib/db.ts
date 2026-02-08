d4f756be61b2c4e3c1cc4b35e9d48d37c609ec9e
Faiyaz <sifathhossain115@gmail.com>
2026-02-08 20:06:35 +0600

    Feat: mongodb connection established with connectDB function

--- lib/db.ts
+++ lib/db.ts
@@ -22,3 +22,30 @@ const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 if (!global.mongoose) {
   global.mongoose = cached;
 }
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
