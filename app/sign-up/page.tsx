b8da407fd2385cb3e60492abe0ac668a49d462c4
Faiyaz <sifathhossain115@gmail.com>
2026-02-09 10:35:53 +0600

    Feat: new Card component installed to integrate in sign-up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function SignUp() {
-  return <div>Sign Up page</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rm)] items-center justify-center bg-white p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign UP</CardTitle>
+          <CardDescription>
+            Create an account to start tracking your job application
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
