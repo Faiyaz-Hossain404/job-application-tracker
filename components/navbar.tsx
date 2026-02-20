2ae8628ddf963cb0caeac15bb92aa595367732cf
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-20 16:47:17 +0600

    Feat: user dropdown css updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,7 @@ import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "./ui/avatar";
@@ -38,19 +39,28 @@ export default function Navbar() {
               </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant="ghost">
-                    <Avatar>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full"
+                  >
+                    <Avatar className="h- w-8">
                       <AvatarFallback className="bg-primary text-white">
                         {session.user.name[0].toUpperCase()}
                       </AvatarFallback>
                     </Avatar>
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <div>
-                    <p>{session.user.name}</p>
-                    <p>{session.user.email}</p>
-                  </div>
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {session.user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {session.user.email}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
                   <SignOutButton />
                 </DropdownMenuContent>
               </DropdownMenu>
