0316a4f5f760228094ba6c925581fafc821aa035
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-20 16:37:10 +0600

    Feat:signing out now takes users back to sign in page - error handling added for signing out

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -2,10 +2,18 @@
 
 import { signOut } from "@/lib/auth/auth-client";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export default function SignOutButton() {
+  const router = useRouter();
   return (
-    <DropdownMenuItem onClick={async () => await signOut()}>
+    <DropdownMenuItem
+      onClick={async () => {
+        const result = await signOut();
+        if (result.data) router.push("/sign-in");
+        else alert("Error signing out");
+      }}
+    >
       Log Out
     </DropdownMenuItem>
   );
