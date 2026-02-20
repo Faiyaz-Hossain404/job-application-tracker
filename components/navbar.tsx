437ee08d38d9cd8c9e3a5a7df01288539c4539b4
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-20 14:55:02 +0600

    Feat: navbar now turned into a client component and singout and session functions used from auth-client

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { getSession } from "@/lib/auth/auth";
+// import { getSession } from "@/lib/auth/auth";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,9 +11,10 @@ import {
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import SignOutButton from "./sign-out-button";
+import { useSession } from "@/lib/auth/auth-client";
 
-export default async function Navbar() {
-  const session = await getSession();
+export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { signOut } from "@/lib/auth/auth-client";
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+
 export default function SignOutButton() {
   return (
     <DropdownMenuItem onClick={async () => await signOut()}>
