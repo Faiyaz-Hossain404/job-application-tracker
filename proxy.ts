4c797f60b272415142b8423aad330c809c9c0794
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-21 21:51:45 +0600

    proxy file set up done. no auth users redirected to sign-in page

---
+++ proxy.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/auth/auth";
+
+export default async function proxy(request: NextRequest) {
+  const session = await getSession();
+
+  const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+  if (isDashboardPage && !session?.user) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  //if not show the dashboard page
+  return NextResponse.next();
+}
