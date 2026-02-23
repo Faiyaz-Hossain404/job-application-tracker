5bb83917b1d4ad11f71fed2f02806e45f3a20adf
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-24 01:53:49 +0600

    Fix: middleware function updated. auth users are redirected to dashboard

--- proxy.ts
+++ proxy.ts
@@ -4,11 +4,18 @@ import { getSession } from "./lib/auth/auth";
 export default async function proxy(request: NextRequest) {
   const session = await getSession();
 
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
   const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
   if (isDashboardPage && !session?.user) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  //if not show the dashboard page
+  //continue to work normally
   return NextResponse.next();
 }
