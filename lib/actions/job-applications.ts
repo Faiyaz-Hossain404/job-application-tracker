aaabbfe941b615762d63bc812eb6b6de806461e5
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-26 22:20:18 +0600

    Feat: server action createJobApplication function initialized with data of the application destructured

---
+++ lib/actions/job-applications.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { getSession } from "../auth/auth";
+import connectDB from "../db";
+
+interface JobApplicationData {
+  company: string;
+  position: string;
+  location?: string;
+  notes?: string;
+  salary?: string;
+  jobUrl?: string;
+  columnId: string;
+  boardId?: string;
+  tags?: string[];
+  description?: string;
+}
+
+export async function createJobApplication(data: JobApplicationData) {
+  const session = await getSession();
+
+  if (!session?.user) {
+    return { error: "Unauthorised" };
+  }
+
+  await connectDB();
+
+  const {
+    company,
+    position,
+    location,
+    notes,
+    salary,
+    jobUrl,
+    columnId,
+    boardId,
+    tags,
+    description,
+  } = data;
+}
