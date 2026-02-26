c734b6f36bdab0ace8d96af07893bcef4263cbad
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-26 11:47:26 +0600

    Feat: css added in droppable column component

--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -6,13 +6,18 @@ import {
   Calendar,
   CheckCircle2,
   Mic,
-  MoreHorizontal,
   MoreVertical,
+  Trash2,
   XCircle,
 } from "lucide-react";
 import React from "react";
 import { Card, CardHeader, CardTitle } from "./ui/card";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 
 interface KanbanBoardProps {
@@ -59,19 +64,33 @@ function DroppableColumn({
 }) {
   console.log(column);
   return (
-    <Card>
-      <CardHeader className={`${config.color}`}>
-        <div>
-          <div>
+    <Card className="min-w-75 shrink-0 shadow-md p-0">
+      <CardHeader
+        className={`${config.color} text-white rounded-t-lg pb-3 pt-3`}
+      >
+        <div className="flex itmems-center justify-between">
+          <div className="flex items-center gap-2">
             {config.icon}
-            <CardTitle>{column.name}</CardTitle>
+            <CardTitle className="text-white text-base font-semibold">
+              {column.name}
+            </CardTitle>
           </div>
           <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant="ghost" asChild>
-                <MoreVertical />
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="h-6 w-6 text-white hover:bg:white/20 hover:text-black"
+              >
+                <MoreVertical className="h-4 w-4" />
               </Button>
             </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem className="text-destructive">
+                <Trash2 className="mr-1 h-4 w-4" />
+                Delete Column
+              </DropdownMenuItem>
+            </DropdownMenuContent>
           </DropdownMenu>
         </div>
       </CardHeader>
