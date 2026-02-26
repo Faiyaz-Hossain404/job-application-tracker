bba82c8de30c3f10919602613d341c94868ea7da
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-02-26 15:38:08 +0600

    Feat: dialog box 4 inputs added with css

--- components/create-job-dialogue.tsx
+++ components/create-job-dialogue.tsx
@@ -2,7 +2,16 @@
 
 import { Plus } from "lucide-react";
 import { Button } from "./ui/button";
-import { Dialog, DialogTrigger } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 interface CreateJobApplicationDialogueProps {
   columnId: string;
@@ -17,11 +26,41 @@ export default function CreateJobApplicationDialogue({
     <>
       <Dialog>
         <DialogTrigger>
-          <Button>
-            <Plus />
+          <Button variant="outline" className="w-full mb-4">
+            <Plus className="h-4 w-4 mr-2" />
             Add Job
           </Button>
         </DialogTrigger>
+        <DialogContent className="max-w-xl">
+          <DialogHeader>
+            <DialogTitle>Add Job Application</DialogTitle>
+            <DialogDescription>Track a new job aapplication</DialogDescription>
+          </DialogHeader>
+          <form>
+            <div className="space-y-4">
+              <div className="grid grid-cols-2 gap-2">
+                <div className="space-y-2">
+                  <Label htmlFor="company">Company *</Label>
+                  <Input id="company" required />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="position">Position *</Label>
+                  <Input id="position" required />
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-2">
+                <div className="space-y-2">
+                  <Label htmlFor="location">Location</Label>
+                  <Input id="location" required />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="salary">Salary</Label>
+                  <Input id="salary" placeholder="e.g. $100k-$150k" required />
+                </div>
+              </div>
+            </div>
+          </form>
+        </DialogContent>
       </Dialog>
     </>
   );
