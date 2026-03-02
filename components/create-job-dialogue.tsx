66e3ddb1015819b2cfc2737f1b1f12b77a875190
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-03 01:03:37 +0600

    Feat: job application create function called in job dialog - split, trim and filtered the tags into a clean array of tags - proper error handling added and basic form submition behavior added

--- components/create-job-dialogue.tsx
+++ components/create-job-dialogue.tsx
@@ -15,33 +15,51 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import React, { useState } from "react";
-import connectDB from "@/lib/db";
+import { createJobApplication } from "@/lib/actions/job-applications";
 
 interface CreateJobApplicationDialogueProps {
   columnId: string;
   boardId: string;
 }
 
+const INITIAL_FORM_DATA = {
+  company: "",
+  position: "",
+  location: "",
+  notes: "",
+  salary: "",
+  jobUrl: "",
+  tags: "",
+  description: "",
+};
+
 export default function CreateJobApplicationDialogue({
   columnId,
   boardId,
 }: CreateJobApplicationDialogueProps) {
   const [open, setOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    company: "",
-    position: "",
-    location: "",
-    notes: "",
-    salary: "",
-    jobUrl: "",
-    tags: "",
-    description: "",
-  });
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
 
     try {
+      const result = await createJobApplication({
+        ...formData,
+        columnId,
+        boardId,
+        tags: formData.tags
+          .split(",")
+          .map((tag) => tag.trim())
+          .filter((tag) => tag.length > 0),
+      });
+
+      if (!result.error) {
+        setFormData(INITIAL_FORM_DATA);
+        setOpen(false);
+      } else {
+        console.error("Failed to create job application: ", result.error);
+      }
     } catch (err) {
       console.error(err);
     }
