ff049c4218233153e841b4682fbad08520ec898a
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-02 23:34:24 +0600

    Feat: board ownership and column to board relation checking added

--- lib/actions/job-applications.ts
+++ lib/actions/job-applications.ts
@@ -2,6 +2,7 @@
 
 import { getSession } from "../auth/auth";
 import connectDB from "../db";
+import { Board, Column } from "../models";
 
 interface JobApplicationData {
   company: string;
@@ -40,5 +41,25 @@ export async function createJobApplication(data: JobApplicationData) {
 
   if (!company || !position || !columnId || !boardId) {
     return { error: "Missing required fields" };
+  }
+
+  //verify if the user has ownershipt over the board
+  const board = await Board.findOne({
+    _id: boardId,
+    userId: session.user.id,
+  });
+
+  if (!board) {
+    return { error: "Board not found" };
+  }
+
+  //verify column belongs to the board
+  const column = Column.findOne({
+    _id: columnId,
+    boardId: boardId,
+  });
+
+  if (!column) {
+    return { error: "Column not found" };
   }
 }
