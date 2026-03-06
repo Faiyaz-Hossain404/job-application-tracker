945a256367ed67e2882441eb494c0fef970cee43
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-06 20:15:56 +0600

    Fix: jobapplication type folder and file removed

---
+++ components/job-card.tsx
@@ -0,0 +1,11 @@
+import { Column, JobApplication } from "@/lib/models/models.types";
+
+export default function JobCard({
+  job,
+  column,
+}: {
+  job: JobApplication;
+  column: Column;
+}) {
+  return <div></div>;
+}
